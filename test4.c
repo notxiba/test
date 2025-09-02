61dac9d5f2a271f53a06c6aeec70b532276cc89c
notxiba <yousseftalhane2007@gmail.com>
2025-09-02 23:12:45 +0100

    Add files via upload

---
+++ test4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int number;
+    printf("entre number:");
+    scanf("%i", &number);
+    if(number % 2 == 0){
+        printf("le number est pair./n");
+        
+    }
+    else{
+        printf("le number est impair./n")
+    }
+    return 0;
+    
+}
+    
+    
