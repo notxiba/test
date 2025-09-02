a76f7447f176b8bbee7866c0050cddbadc14ea51
notxiba <yousseftalhane2007@gmail.com>
2025-09-02 23:34:16 +0100

    Add files via upload

---
+++ test5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int a, b;
+    printf("entre  number: ");
+    scanf("%d", &a);
+    printf("%d", &b);
+    somme = a + b
+    if( a == b){
+        printf("les nomber et identique./n ");
+        printf("la som = %d./n" , 3 * somme);
+    }
+    else{
+        printf("la som = %d./n", somme);
+    }
+    return 0;
+}
